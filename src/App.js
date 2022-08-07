243893545b92ca19828ae77225b0e193bf38e43b
Reander Agulto <reanderagulto29@gmail.com>
2022-08-08 06:13:18 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ todos, setTodos ] = useState(['Take Dogs for a walk', 'Take the rubbish out', 'Study React']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Clever Programmers !!</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        {
+          todos.map(todo => (
+            <li>{ todo }</li>
+          ))
+        }
+      </ul>
+      
     </div>
   );
 }
