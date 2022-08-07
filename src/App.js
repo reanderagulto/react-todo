00e91077d0ad8f0d82ae891e8b9830541cf9d67a
Reander Agulto <reanderagulto29@gmail.com>
2022-08-08 06:31:45 +0800

    UseState Sample

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 function App() {
 
   const [ todos, setTodos ] = useState(['Take Dogs for a walk', 'Take the rubbish out', 'Study React']);
+  const [ input, setInput ] = useState('');
+  
+  const addTodo = (event) => {
+    // This will fire off when we click the button 
+    setTodos([...todos, input]); 
+    setInput('');
+  }
 
   return (
     <div className="App">
       <h1>Hello Clever Programmers !!</h1>
-      <input />
-      <button>Add Todo</button>
-
+      <input value={input} onChange={ event => setInput( event.target.value )} />
+      <button onClick={ addTodo }>Add Todo</button>
       <ul>
         {
           todos.map(todo => (
@@ -18,7 +24,6 @@ function App() {
           ))
         }
       </ul>
-      
     </div>
   );
 }
