a7ded92cdcf56d0d61651c3582e9507918855589
Reander Agulto <reanderagulto29@gmail.com>
2022-08-08 22:16:50 +0800

    Added MUI

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import React, { useState } from 'react';
+import { Button, FormControl, InputLabel, Input } from '@mui/material';
 import './App.css';
+import Todo from './Todo';
 
 function App() {
 
-  const [ todos, setTodos ] = useState(['Take Dogs for a walk', 'Take the rubbish out', 'Study React']);
-  const [ input, setInput ] = useState('');
-  
-  const addTodo = (event) => {
-    // This will fire off when we click the button 
-    setTodos([...todos, input]); 
-    setInput('');
-  }
+const [ todos, setTodos ] = useState(['Take Dogs for a walk', 'Take the rubbish out', 'Study React']);
+const [ input, setInput ] = useState('');
 
-  return (
-    <div className="App">
-      <h1>Hello Clever Programmers !!</h1>
-      <input value={input} onChange={ event => setInput( event.target.value )} />
-      <button onClick={ addTodo }>Add Todo</button>
-      <ul>
-        {
-          todos.map(todo => (
-            <li>{ todo }</li>
-          ))
-        }
-      </ul>
-    </div>
+const addTodo = (event) => {
+  // This will fire off when we click the button 
+  event.preventDefault();
+  setTodos([...todos, input]); 
+  setInput('');
+}
+
+return (
+  <div className="App">
+    <h1>Hello Clever Programmers !!</h1>
+    <form>
+      <FormControl>
+        <InputLabel>Write a Todo</InputLabel>
+        <Input onChange={ event => setInput( event.target.value )} value={input} label="Todo" variant="outlined" />
+      </FormControl>
+      <Button type="submit" disabled={ !input } onClick={ addTodo } variant="contained">
+      Add Todo
+      </Button>   
+    </form>
+    <ul>
+    {
+      todos.map(todo => (
+        <Todo text={todo}/>
+        
+        // <li>{ todo }</li>
+      ))
+    }
+    </ul>
+  </div>
   );
 }
 
